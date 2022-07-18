2a30b8e2dc159d591757f19175fb770de2fd4d0b
Navid <navid@nourbakhsh.net>
2022-07-18 23:03:45 +0200

    Update heise.de-mark-visited-articles.user.js
    Update URL added and using duckduckgo for favicons

--- heise.de-mark-visited-articles.user.js
+++ heise.de-mark-visited-articles.user.js
@@ -1,11 +1,13 @@
 // ==UserScript==
 // @name         heise.de mark visited articles
 // @namespace    http://tampermonkey.net/
-// @version      1.0
-// @description  Besuchte Artikel auf heise.de Grau markieren
+// @version      1.1
+// @description  Besuchte Artikel auf heise.de in grau markieren
 // @author       Navid
+// @downloadURL  https://github.com/nourbakhsh/userscript-heise-mark-visited-articles/raw/main/heise.de-mark-visited-articles.user.js
+// @updateURL    https://github.com/nourbakhsh/userscript-heise-mark-visited-articles/raw/main/heise.de-mark-visited-articles.user.js
 // @match        https://*.heise.de/*
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=heise.de
+// @icon         https://icons.duckduckgo.com/ip3/heise.de.ico
 // @grant        GM_addStyle
 // ==/UserScript==
 
